727d2443e791116322295e3cb5b63b667894efe4
zkbitcoin <github@zkbitcoin.com>
2022-02-01 23:56:12 -0500

    type errors correction for strict=true

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,11 +1,16 @@
 import '../styles/globals.css';
 import { ChakraProvider } from '@chakra-ui/react';
-function MyApp({ Component, pageProps }) {
-  return (
-    <ChakraProvider>
-      <Component {...pageProps} />
-    </ChakraProvider>
-  );
+import {Component} from "react";
+
+class MyApp extends Component<{ Component: any, pageProps: any }> {
+    render() {
+        let {Component, pageProps} = this.props;
+        return (
+            <ChakraProvider>
+                <Component {...pageProps} />
+            </ChakraProvider>
+        );
+    }
 }
 
 export default MyApp;
